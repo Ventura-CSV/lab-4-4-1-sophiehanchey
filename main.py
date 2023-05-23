37ddd6b4a66965667d32a8a300d1d7b5dfbd28cc
Kyu Lee <kwooler@gmail.com>
2023-05-22 23:43:16 -0700

    Lab 4-4-1

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ def main():
     Code Your Program here
     ########################################
     """
-    number = 0
-    while number <= 0 or number >= 100:
-        number = int(input('Enter your input: '))
 
     print(number)
 
