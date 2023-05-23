d586e3dfc9d51d79b862329539ae7c3a1bf7f5a8
Kyu Lee <kwooler@gmail.com>
2023-05-22 23:43:02 -0700

    Lab 4-4-1

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+    number = 0
+    while number <= 0 or number >= 100:
+        number = int(input('Enter your input: '))
+
+    print(number)
+
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    return number
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -10,15 +10,16 @@ def test_main_1():
     datastr = '150\n200\n30'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
+    assert ret == 30
 
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*30[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
+    # regex_string = r'[\w,\W]*30[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    # assert res != None
+    # print(res.group())
