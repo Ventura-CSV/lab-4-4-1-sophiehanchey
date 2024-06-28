830e9d89a47ab73fe0e5987b824c14b8e647cb67
Sophie Hanchey <sophiehanchey@gmail.com>
2024-06-28 12:11:06 -0700

    fixed to <= and >=

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     number = int(input('Please input a number in the range: '))
     
-    while number<0 or number>100:
+    while number<=0 or number>=100:
         number = int(input('Please enter a valid number: '))
 
     print(number)
