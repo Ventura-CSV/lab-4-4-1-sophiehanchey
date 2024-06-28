5c3dda4e1ee0f65994e17f524c4135c1b34b4805
Sophie Hanchey <sophiehanchey@gmail.com>
2024-06-28 12:10:28 -0700

    initial code, pytest passed

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 def main():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    number = int(input('Please input a number in the range: '))
+    
+    while number<0 or number>100:
+        number = int(input('Please enter a valid number: '))
 
     print(number)
 
